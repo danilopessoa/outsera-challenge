c9d7742c6ad6029d7bd3f263eb80bb70a98df272
Danilo Pessoa <danilosilvapessoa@gmail.com>
2025-10-28 10:14:29 -0300

    feat: adicionando react-query e ajustes de eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import prettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-      tseslint.configs.strictTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -25,5 +22,30 @@ export default defineConfig([
         tsconfigRootDir: import.meta.dirname,
       },
     },
+
+    plugins: {
+      react,
+      reactHooks,
+      reactRefresh,
+      pluginQuery,
+      prettier,
+    },
+
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      ...pluginQuery.configs["flat/recommended"],
+      configPrettier,
+    ],
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "error",
+    }
   },
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Template } from "./template/Template";
 import { Skeleton } from "./components/Skeleton/Skeleton.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
 const Movies = React.lazy(() => import("./pages/Movies/Movies"));
 
-const menus = [
+const routes = [
   { label: "Dashboard", to: "/", component: <Dashboard /> },
   { label: "Filmes", to: "/filmes", component: <Movies /> },
 ];
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Template />}>
-          {menus.map((item) => (
-            <Route
-              key={item.to}
-              path={item.to}
-              element={<React.Suspense fallback={<Skeleton />}>{item.component}</React.Suspense>}
-            />
-          ))}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Template />}>
+            {routes.map((item) => (
+              <Route
+                key={item.to}
+                path={item.to}
+                element={<React.Suspense fallback={<Skeleton />}>{item.component}</React.Suspense>}
+              />
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>{" "}
+    </QueryClientProvider>
   );
 };
 
